0480dacc3a9ad7178221b0a883e12c7d0524d908
ZHUF <zhufeng314@163.com>
2017-11-29 22:34:33 +0800

    '增加从数据和参数来构造第四类数据的构造方法'

--- Diamond4.py
+++ Diamond4.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 
-from datetime import datetime, timedelta
+import datetime
 from math import sqrt, fabs
+import os
+import numpy as np
 
 
 class Diamond4(object):
@@ -10,47 +12,85 @@ class Diamond4(object):
     """
     diamond = 4
 
-    def __init__(self, file_path):
-        with open(file_path, 'r') as f:
-            data_raw = [word for line in f.readlines() if line[:-1].strip()
-                        for word in line.split()]  # 去除空行读入,将原文件分割成一维字符串数组
+    def __init__(self, data_frame, data_parameters=None):
 
-            self.doc = data_raw[2].decode('gbk')  # 说明字符串
+        if data_parameters is None and os.path.isfile(data_frame):
+            with open(data_frame, 'r') as f:
+                data_raw = [word for line in f.readlines() if line[:-1].strip()
+                            for word in line.split()]  # 去除空行读入,将原文件分割成一维字符串数组
 
-            (self.size_lon,  # 经度（x方向）格距, 一般为正
-             self.size_lat,  # 纬度（y方向）格距，有正负号
-             self.lon_start,  # 起始经度
-             self.lon_end,  # 终止经度
-             self.lat_start,  # 起始纬度
-             self.lat_end) = (float(i) for i in data_raw[9:15])  # 终止纬度
+                self.doc = data_raw[2]#.decode('gbk')  # 说明字符串
 
-            (self.cols,  # 纬向(x方向)格点数目，即列数
-             self.rows) = (int(i) for i in data_raw[15:17])  # 经向(y方向)格点数目，即行数
+                # 日期时间处理
+                (year,  # 年
+                 self.month,  # 月
+                 self.day,  # 日
+                 self.hour,  # 时
+                 self.valid_period,  # 时效
+                 self.level) = data_raw[3:9]  # 层次
 
-            # 日期时间处理
-            (month, day, hour, interval) = data_raw[4:8]
-            year = data_raw[3]
-            if len(year) == 2:
-                year = ('20' + year) if int(year) < 49 else ('19' + year)
-            elif len(year) == 4:
-                pass
-            else:
-                raise Exception('year parameter error!')
+                if len(year) == 2:
+                    year = ('20' + year) if int(year) < 49 else ('19' + year)
+                elif len(year) == 4:
+                    pass
+                else:
+                    raise Exception('year parameter error!')
 
-            # 注意start_time和valid_time没有统一规定，要看具体情况
-            self.start_time = datetime(int(year), int(month), int(day), int(hour))
-            self.valid_time = self.start_time + timedelta(hours=int(interval))
+                self.year = year
 
-            # 数据部分，以一维数组表示
-            self.data = [float(i) for i in data_raw[22:]]
+                # 注意start_time和valid_time没有统一规定，要看具体情况
+                self.start_time = datetime.datetime(int(year), int(self.month), int(self.day), int(self.hour))
+                self.valid_time = self.start_time + datetime.timedelta(hours=int(self.valid_period))
 
-            del data_raw
+                (self.size_lon,  # 经度（x方向）格距, 一般为正
+                 self.size_lat,  # 纬度（y方向）格距，有正负号
+                 self.lon_start,  # 起始经度
+                 self.lon_end,  # 终止经度
+                 self.lat_start,  # 起始纬度
+                 self.lat_end) = (float(i) for i in data_raw[9:15])  # 终止纬度
+
+                (self.cols,  # 纬向(x方向)格点数目，即列数
+                 self.rows) = (int(i) for i in data_raw[15:17])  # 经向(y方向)格点数目，即行数
+
+                (self.contour_interval,  # 等值线间隔
+                 self.contour_start,  # 等值线起始值
+                 self.contour_end,  # 等值线终止值
+                 self.smooth,  # 平滑系数
+                 self.bold_line) = (float(i) for i in data_raw[17:22])  # 加粗线值
+
+                # 数据部分，以一维数组表示
+                self.data = [float(i) for i in data_raw[22:]]
+
+                # 将数据的一些属性参数集合到一个字典中
+                self.parameters = {'doc': self.doc, 'year': self.year, 'month': self.month, 'day': self.day,
+                                   'hour': self.hour,'valid_period': self.valid_period,'start_time': self.start_time,
+                                   'valid_time': self.valid_time,'size_lon': self.size_lon, 'size_lat': self.size_lat,
+                                   'lon_start': self.lon_start, 'lon_end': self.lon_end, 'lat_start': self.lat_start,
+                                   'lat_end': self.lat_end, 'cols': self.cols, 'rows': self.rows,
+                                   'contour_interval': self.contour_interval, 'contour_start': self.contour_start,
+                                   'contour_end': self.contour_end, 'smooth': self.smooth, 'bold_line': self.bold_line}
+                del data_raw
+
+        elif data_parameters is not None and isinstance(data_frame, (np.ndarray, list)):
+
+            self.data = data_frame if isinstance(data_frame, list) else data_frame.flatten().tolist()
+            self.parameters = data_parameters
+            for key in data_parameters:
+                exec('self.'+ key + '=' + repr(data_parameters[key]))
+
+        else:
+            raise Exception('input parameters error!')
 
     def __getitem__(self, index):
         if isinstance(index, tuple) and len(index) == 2:
             return self.value(index[0], index[1])
         else:
             raise Exception('indexing error!')
+
+    def __sub__(self, other):
+        if self.rows == other.rows and self.cols == self.cols:
+            return [x - y for x, y in zip(self.data, other.data)]
+
 
     def value(self, row, col):
         '''将格点数据看成self.cols*self.nums_lat的二维数组，返回第row行，第col列的值，
@@ -103,7 +143,7 @@ class Diamond4(object):
         '提取站点数据'
         pass
 
-    def convert_to_EsriAscii(self, out_name):
+    def to_esri_ascii(self, out_name):
         with open(out_name, 'w') as f:
             y_start = self.lat_end if self.size_lat < 0  else self.lat_start
             header = 'NCOLS %d\nNROWS %d\nXLLCENTER %f\nYLLCENTER %f\nCELLSIZE %f\nNODATA_VALUE 9999.0\n' % (
@@ -119,7 +159,7 @@ class Diamond4(object):
         try:
             import arcpy
         except ImportError:
-            print("""warning: you have no Esri's arcpy module, using convert_to_EsriAsii function,
+            print("""warning: you have no Esri's arcpy module, using to_esri_ascii function,
                     you can still get the result, but without the associate coordinate information.
                     you can use Esri's software like Arcmap to import the result and add the coordinate which is
                     WGS1984""")
@@ -129,18 +169,24 @@ class Diamond4(object):
             arcpy.DefineProjection_management(out_name, sr)
 
     def write_to_diamond4_txt(self, out_name):
+        # with open(out_name) as f:
+        #     # year = self.start_time.strftime('%y')
+        #     # month =
+        #     f.write('diamond4 ' + self.doc + '\n')
+        #     f.write(' '.join([self.]))
         pass
 
     def calc_stats(self):
         pass
 
     def to_numpy(self):
-        try:
-            import numpy as np
-        except ImportError:
-            'you have no numpy module'
-        else:
-            return np.array(self.data).reshape(self.rows, self.cols)
+        return np.array(self.data).reshape(self.rows, self.cols)
+
 
 if __name__ == "__main__":
     d = Diamond4('D:/000')
+    print(d.doc)
+    d.doc = '换句话'
+    print(d.parameters['doc'])
+    #todo 属性改变需要引起self.parameter改变
+    print(d.doc)
