89d5470e53070ce8884cda44f800747cc4e12c62
ZHUF <zhufeng314@163.com>
2017-12-08 18:10:49 +0800

    更改GDSDataService部分方法名

--- GDSDataService.py
+++ GDSDataService.py
@@ -36,11 +36,11 @@ class GDSDataService:
         url += "&filter=" + filter
         return url
 
-    def getLatestDataName(self, directory, filter):
-        "获取directory目录下最近数据文件名"
+    def get_latest_data_name(self, directory, filter):
+        "获取directory目录下最近数据文件名,如果返回None,表示获取失败"
 
         status, response = get_http_result(self.gdsIp, self.gdsPort, "/DataService" +
-                               self._get_concate_url("getLatestDataName", directory, "", filter))
+                                           self._get_concate_url("getLatestDataName", directory, "", filter))
         StringResult = DataBlock_pb2.StringResult()
 
         if status == 200:
@@ -53,11 +53,11 @@ class GDSDataService:
         else:
             return None
 
-    def getFileList(self, directory, filter):
-        "获取directory目录下以filter开头的所有文件名组成的列表"
+    def get_file_list(self, directory, filter=''):
+        "获取directory目录下以filter开头的所有文件名组成的列表,如果返回None,表示获取失败"
 
         status, response = get_http_result(self.gdsIp, self.gdsPort, "/DataService" +
-                                                    self._get_concate_url("getFileList", directory, "", ""))
+                                           self._get_concate_url("getFileList", directory, "", ""))
         MappingResult = DataBlock_pb2.MapResult()
 
         if status == 200:
@@ -67,18 +67,17 @@ class GDSDataService:
                 file_list_results = MappingResult.resultMap
 
                 file_list = (file for file in file_list_results if file.startswith(filter))
-
                 return file_list
             else:
                 return None
         else:
             return None
 
-    def getData(self, directory, file_name):
-        "获取directory目录下文件名为file_name的二进制数据"
+    def get_data(self, directory, file_name):
+        "获取directory目录下文件名为file_name的二进制数据,如果返回None,表示获取数据失败"
 
         status, response = get_http_result(self.gdsIp, self.gdsPort, "/DataService" +
-                                                    self._get_concate_url("getData", directory, file_name, ""))
+                                           self._get_concate_url("getData", directory, file_name, ""))
         ByteArrayResult = DataBlock_pb2.ByteArrayResult()
 
         if status == 200:
@@ -91,17 +90,18 @@ class GDSDataService:
         else:
             return None
 
-    def download_data(self, output_directory, directory, filter):
+    def bulk_download(self, output_directory, directory, filter):
         "批量下载directory目录下文件名以filter开头的数据文件，下载保存目录为output_directory"
 
-        file_list = self.getFileList(directory,filter)
+        file_list = self.get_file_list(directory, filter)
         for file_name in file_list:
-            byteArray = self.getData(directory,file_name)
+            byteArray = self.get_data(directory, file_name)
             with open(os.path.join(output_directory, file_name), 'wb') as f:
                 f.write(byteArray)
 
 
-
 if __name__ == '__main__':
     gds = GDSDataService("10.69.72.112", 8080)
-    gds.download_data('D:/ECMWF','ECMWF_HR/TMP/850/','17120808')
+    # gds.download_data('D:/ECMWF','ECMWF_HR/TMP/850/','17120720')
+    for i in gds.get_file_list("ECMWF_HR"):
+        print(i)
