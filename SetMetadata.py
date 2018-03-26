aeeb7d21c43865de9b3c9c427bf9d581d2037d5d
zhuf <zhufeng314@163.com>
2018-03-26 08:28:22 +0800

    大幅度修改，改进GDSDataService类，增强其功能，比如目录遍历，多线程下载

--- SetMetadata.py
+++ SetMetadata.py
@@ -3,9 +3,12 @@
 from GDSDataService import GDSDataService
 import xml.etree.ElementTree as ET
 import os, time
-
+from datetime import datetime,timedelta
 
 def get_mdfs_data_dirs(gds, source, no_unclipped=True):
+    """
+    获取分布式micaps中source数据的下的目录，默认不包含unclipped的目录
+    """
     result = {}
     l = list(gds.get_file_list(source))  # 第一级目录
     if no_unclipped:
@@ -13,6 +16,7 @@ def get_mdfs_data_dirs(gds, source, no_unclipped=True):
     l.sort()
 
     for dir in l:
+        #todo 修改判断目录的方法，修改获取两级以上的目录问题
         if gds.get_latest_data_name(os.path.join(source, dir), '*.024') != "" or gds.get_latest_data_name(
                 os.path.join(source, dir), '*.000') != "":  # 没有第二级目录
 
@@ -31,6 +35,9 @@ def get_mdfs_data_dirs(gds, source, no_unclipped=True):
 
 
 def create_data_node(gds, data_source, data_name):
+    """
+    创建data_source目录的xml节点
+    """
     description = {}
     with open(data_source, 'r', encoding='utf-8') as f:
         for line in f:
@@ -81,7 +88,7 @@ def create_data_node(gds, data_source, data_name):
         if dir1 in (
                 # EC
                 'SPECIFIC_CLOUD_ICE_WATER_CONTENT', 'SPECIFIC_CLOUD_LIQUID_WATER_CONTENT', 'FRACTION_OF_CLOUD_COVER',
-                'UGRD_100M', 'UGRD_100M', '10_METRE_WIND_GUST_IN_THE_LAST_6_HOURS', 'UGRD_10M', 'VGRD_10M', 'UGRD',
+                'UGRD_100M', 'VGRD_100M', '10_METRE_WIND_GUST_IN_THE_LAST_6_HOURS', 'UGRD_10M', 'VGRD_10M', 'UGRD',
                 'VGRD', 'OROGRAPHY', 'ALBEDO', 'MAXIMUM_TEMPERATURE_AT_2_METRES_IN_THE_LAST_6_HOURS',
                 'MINIMUM_TEMPERATURE_AT_2_METRES_IN_THE_LAST_6_HOURS', 'NATURAL_LOGARITHM_OF_PRESSURE_IN_PA',
                 # GRAPES_GFS
@@ -147,6 +154,120 @@ def write_to_xml(gds, out_file='micapsdata.xml'):
     tree.write(out_file, 'utf-8', True, short_empty_elements=False)
 
 
+def get_time_resolution(L):
+    if len(L)==1:
+        return str(L[0])
+
+    s = ''
+    start = L[0]
+    interval = L[1] - L[0]
+    for i in range(2, len(L)):
+        if L[i] - L[i - 1] != interval:
+            end = L[i - 1]
+            s += '%s %s %s,' % (start, end, interval)
+            start = L[i]
+            interval = L[i] - L[i - 1]
+        if i == len(L) - 1:
+            end = L[i]
+            s += '%s %s %s,' % (start, end, interval)
+
+    return s[:-1]
+
+
+def parse_time_resolution(s):
+    for i in s.split(','):
+        start, end, interval = [int(each) for each in i.split()]
+        for j in range(start, end + interval, interval):
+            yield '%03d' % int(j)
+
+
+def get_directory(self, data_type,get_type, month, config='micapsdata.xml'):
+
+    root = ET.parse(config)
+    data_node = root.find("./MODEL_DATA[@name='%s']" % data_type)
+    for first_dir in data_node.iterfind("./FIRST_DIR"):
+        if first_dir.attrib['%s'%get_type]=='True' or month in first_dir.attrib['%s'%get_type].split(','):
+            if not list(first_dir):#没有二级目录
+                directory = os.path.join(data_type, first_dir.text)
+                yield directory
+            for second_dir in first_dir.iterfind("./SECOND_DIR[@download='True']"):
+                if second_dir.attrib['%s'%get_type]=='True' or month in second_dir.attrib['%s'%get_type].split(','):
+                    directory = os.path.join(data_type, first_dir.text, second_dir.text)
+                    yield directory
+
+
+def default_start_predict_time(year_label='y'):
+    """
+    以'yymmddhh'
+    """
+    now = datetime.now()
+    today = now.strftime('%'+year_label+'%m%d')
+    nowtime = now.strftime('%'+year_label+'%m%d%H')
+    if nowtime < today + "12":
+        yesterday = now - timedelta(days=1)
+        start_predict = yesterday.strftime('%'+year_label+'%m%d') + '20'
+    else:
+        start_predict = today + '08'
+    return start_predict
+
+def modify_xml_time_resolution(gds, config='micapsdata.xml'):
+
+    now = datetime.now()
+    times = [(now -timedelta(days=day)).strftime('%y%m%d') + '20' for day in range(1,4)]
+
+    root = ET.parse(config)
+
+    #注意日本08 20时间不一样
+    data = ['JAPAN_HR']#['ECMWF_HR','GERMAN_HR','GRAPES_GFS','GRAPES_MESO_HR','JAPAN_HR','SHANGHAI_HR','T639']
+    for data_type in data:
+        data_node = root.find("./MODEL_DATA[@name='%s']" % data_type)
+        for first_dir in data_node.iterfind("./FIRST_DIR"):
+            if not list(first_dir):#没有二级目录
+                directory = os.path.join(data_type, first_dir.text)
+
+                Ls = []
+                for t in times:
+                    L = [int(each[-3:]) for each in gds.get_file_list(directory,t)]
+                    L.sort()
+                    Ls.append(L)
+
+                L = Ls[0]
+                if not Ls[0]==Ls[1]==Ls[2]:
+                    print(data_type, first_dir.text, 'error:*************')
+                    print(Ls[0],Ls[1],Ls[2])
+                    if len(Ls[1])>len(L):
+                        L=Ls[1]
+                    if len(Ls[2])>len(L):
+                        L=Ls[2]
+                    #raise Exception('时间分辨率判断有误，请检查！')
+
+                first_dir.attrib['time_resolution'] = get_time_resolution(L)
+
+            else:# 有二级目录
+                for second_dir in first_dir.iterfind("./SECOND_DIR"):
+
+                    directory = os.path.join(data_type, first_dir.text, second_dir.text)
+
+                    Ls = []
+                    for t in times:
+                        L = [int(each[-3:]) for each in gds.get_file_list(directory,t)]
+                        L.sort()
+                        Ls.append(L)
+
+                    L = Ls[0]
+                    if not Ls[0]==Ls[1]==Ls[2]:
+                        print(data_type, first_dir.text, second_dir.text, 'error:*************')
+                        print(Ls[0],Ls[1],Ls[2])
+                        #raise Exception('时间分辨率判断有误，请检查！')
+                        if len(Ls[1])>len(L):
+                            L=Ls[1]
+                        if len(Ls[2])>len(L):
+                            L=Ls[2]
+
+                    second_dir.attrib['time_resolution'] = get_time_resolution(L)
+
+    root.write(config, 'utf-8', True, short_empty_elements=False)
+
 if __name__ == '__main__':
     with GDSDataService("10.69.72.112", 8080) as gds:
         start = time.clock()
@@ -158,8 +279,11 @@ if __name__ == '__main__':
         #     else:
         #         s = '/'.join(dirs[key])
         #         print(key, s)
-        write_to_xml(gds)
 
+        #write_to_xml(gds)
+
+        #modify_xml_time_resolution(gds)
+        print(default_start_predict_time('Y'))
         # ***********************测试程序*********************************"
         end = time.clock()
         elapsed = end - start
